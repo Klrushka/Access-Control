72700b7cb63cdc0069d827909f98193eec0446db
Klrushka <76685224+Klrushka@users.noreply.github.com>
2021-06-07 11:46:21 +0300

    Add files via upload

---
+++ AccessControl/src/debug/Debug.java
@@ -0,0 +1,8 @@
+package debug;
+
+public class Debug {
+
+    public static void  debug (String str) {
+        System.out.println("String from params: " + str);
+    }
+}
---
+++ AccessControl/src/task4Pac/Task4.java
@@ -0,0 +1,7 @@
+package task4Pac;
+
+public class Task4 {
+    protected static void test(){
+
+    }
+}
